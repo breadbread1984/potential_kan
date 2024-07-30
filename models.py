1c3f6320da19e7fe9f5a7cadeda4439ead24bd1e
breadbread1984 <breadbread1984@163.com>
2024-07-30 01:03:23 +0000

    update

--- models.py
+++ models.py
@@ -3,6 +3,132 @@
 import torch
 from torch import nn
 
+class SwitchGate(nn.Module):
+
+    def __init__(
+        self,
+        dim,
+        num_experts: int,
+        capacity_factor: float = 1.0,
+        epsilon: float = 1e-6,
+        *args,
+        **kwargs,
+    ):
+        super().__init__()
+        self.dim = dim
+        self.num_experts = num_experts
+        self.capacity_factor = capacity_factor
+        self.epsilon = epsilon
+        self.w_gate = nn.Linear(dim, num_experts)
+
+    def forward(self, x: torch.Tensor, use_aux_loss=False):
+
+        # Compute gate scores
+        gate_scores = F.softmax(self.w_gate(x), dim=-1)
+
+        # Determine the top-1 expert for each token
+        capacity = int(self.capacity_factor * x.size(0))
+
+        top_k_scores, top_k_indices = gate_scores.topk(1, dim=-1)
+
+        # Mask to enforce sparsity
+        mask = torch.zeros_like(gate_scores).scatter_(
+            1, top_k_indices, 1
+        )
+
+        # Combine gating scores with the mask
+        masked_gate_scores = gate_scores * mask
+
+        # Denominators
+        denominators = (
+            masked_gate_scores.sum(0, keepdim=True) + self.epsilon
+        )
+
+        # Norm gate scores to sum to the capacity
+        gate_scores = (masked_gate_scores / denominators) * capacity
+
+        if use_aux_loss:
+            load = gate_scores.sum(0)  # Sum over all examples
+            importance = gate_scores.sum(1)  # Sum over all experts
+
+            # Aux loss is mean suqared difference between load and importance
+            loss = ((load - importance) ** 2).mean()
+
+            return gate_scores, loss
+
+        return gate_scores, None
+
+class SwitchMoE(nn.Module):
+
+    def __init__(
+        self,
+        dim: int,
+        hidden_dim: int,
+        output_dim: int,
+        num_experts: int,
+        capacity_factor: float = 1.0,
+        mult: int = 4,
+        use_aux_loss: bool = False,
+        *args,
+        **kwargs,
+    ):
+        super().__init__()
+        self.dim = dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.num_experts = num_experts
+        self.capacity_factor = capacity_factor
+        self.mult = mult
+        self.use_aux_loss = use_aux_loss
+
+        self.experts = nn.ModuleList(
+            [
+                nn.Sequential(
+                    nn.Linear(self.dim, self.dim * self.mult, bias = True),
+                    nn.GELU(),
+                    nn.Dropout(0.1),
+                    nn.Linear(self.dim * self.mult, self.dim, bias = True))
+                for _ in range(num_experts)
+            ]
+        )
+
+        self.gate = SwitchGate(
+            dim,
+            num_experts,
+            capacity_factor,
+        )
+
+    def forward(self, x: torch.Tensor):
+
+        # (batch_size, seq_len, num_experts)
+        gate_scores, loss = self.gate(
+            x, use_aux_loss=self.use_aux_loss
+        )
+
+        # Dispatch to experts
+        expert_outputs = [expert(x) for expert in self.experts]
+
+        # Check if any gate scores are nan and handle
+        if torch.isnan(gate_scores).any():
+            print("NaN in gate scores")
+            gate_scores[torch.isnan(gate_scores)] = 0
+
+        # Stack and weight outputs
+        stacked_expert_outputs = torch.stack(
+            expert_outputs, dim=-1
+        )  # (batch_size, seq_len, output_dim, num_experts)
+        if torch.isnan(stacked_expert_outputs).any():
+            stacked_expert_outputs[
+                torch.isnan(stacked_expert_outputs)
+            ] = 0
+
+        # Combine expert outputs and gating scores
+        moe_output = torch.sum(
+            gate_scores.unsqueeze(-2) * stacked_expert_outputs, dim=-1
+        )
+
+        return moe_output, loss
+
 class MLP(nn.Module):
   def __init__(self,):
     super(MLP, self).__init__()
@@ -12,7 +138,7 @@ class MLP(nn.Module):
       nn.Dropout(),
       nn.GELU(),
       nn.BatchNorm1d(8),
-      nn.Linear(8,4),
+      SwitchMoE(8, 32, 8, 3),
       nn.Dropout(),
       nn.GELU(),
       nn.BatchNorm1d(4),
