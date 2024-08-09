ffe5270182cd8501b2b9cfeb7729dae391ce568d
breadbread1984 <breadbread1984@163.com>
2024-08-09 17:00:00 +0800

    update

--- create_dataset.py
+++ create_dataset.py
@@ -16,15 +16,15 @@ class RhoDataset(Dataset):
         if ext != '.npz': continue
         self.npys.append(np.load(join(root, f), mmap_mode = 'r'))
         self.start_indices.append(count)
-        self.data_count += np.prod(self.npys[-1]['rho_inv_4_norm'].shape[1:])
+        self.data_count += np.prod(self.npys[-1]['rho_inv_4_norm'].shape[1:3])
   def __len__(self):
     return self.data_count
   def __getitem__(self, index):
     memmap_index = bisect(self.start_indices, index) - 1
     index_in_memmap = index - self.start_indices[memmap_index]
-    rho = self.npys[memmap_index]['rho_inv_4_norm'].reshape((4,-1))[index_in_memmap] # data.shape = (4,)
-    vxc = self.npys[memmap_index]['vxc1_b3lyp'].reshape((1,-1))[index_in_memmap] # vxc.shape = (1,)
-    exc = self.npys[memmap_index]['exc1_tr_b3lyp'].reshape((1,-1))[index_in_memmap] # exc.shape = (1,)
+    rho = self.npys[memmap_index]['rho_inv_4_norm'][0,...].reshape((5 * 75, 302))[index_in_memmap] # data.shape = (302,)
+    vxc = self.npys[memmap_index]['vxc1_b3lyp'][0,...].reshape((5 * 75, 302))[index_in_memmap] # vxc.shape = (302,)
+    exc = self.npys[memmap_index]['exc1_tr_b3lyp'][0,...].reshape((5 * 75, 302))[index_in_memmap] # exc.shape = (302,)
     return rho, vxc, exc
 
 if __name__=="__main__":
